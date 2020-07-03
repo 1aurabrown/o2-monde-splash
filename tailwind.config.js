b3d41cf9064b296075b033d9f66cdd1d0d4cbb59
Laura Brown <laura@laurabrown.xyz>
2020-07-03 17:39:41 +0200

    update tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,50 @@
 module.exports = {
-
+  theme: {
+    borderWidth: {
+      default: '1px',
+      '0': '0'
+    },
+    colors: {
+      backdrop: 'rgba(10, 24, 27, .15)',
+      white: '#ffffff'
+    }
+  },
+  corePlugins: {
+    float: false,
+    clear: false,
+    objectPosition: false,
+    alignSelf: false,
+    gridColumnStart: false,
+    gridColumnEnd: false,
+    gridAutoFlow: false,
+    fontStyle: false,
+    fontWeight: false,
+    letterSpacing: false,
+    listStyleType: false,
+    listStylePosition: false,
+    placeholderColor: false,
+    placeholderOpacity: false,
+    textDecoration: false,
+    textTransform: false,
+    wordBreak: false,
+    backgroundAttachment: false,
+    backgroundPosition: false,
+    backgroundRepeat: false,
+    backgroundSize: false,
+    borderStyle: false,
+    divideWidth: false,
+    divideColor: false,
+    divideOpacity: false,
+    borderCollapse: false,
+    tableLayout: false,
+    boxShadow: false,
+    scale: false,
+    skew: false,
+    transformOrigin: false,
+    resize: false,
+    userSelect: false,
+    strokeWidth: false,
+    fill: false,
+    stroke: false
+  }
 }
